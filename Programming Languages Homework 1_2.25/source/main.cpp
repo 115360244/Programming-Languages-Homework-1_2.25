70a03ce2cef6cc6a0e57a95bc23d605b89ebe02d
115360244 <t115360244@ntut.org.tw>
2026-09-21 22:26:33 +0800

    Add files via upload

---
+++ Programming Languages Homework 1_2.25/source/main.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    cout << "PPPPPPPPP\n";
+    cout << "    P    P\n";
+    cout << "    P    P\n";
+    cout << "    P    P\n";
+    cout << "     P  P\n";
+    cout << "\n"; 
+
+    cout << "   JJ    \n";
+    cout << "  J      \n";
+    cout << " J       \n";
+    cout << "  J      \n";
+    cout << "   JJJJJJJ\n";
+    cout << "\n";
+
+    cout << "DDDDDDDDD\n";
+    cout << "D       D\n";
+    cout << "D       D\n";
+    cout << " D     D \n";
+    cout << "  DDDDD  \n";
+    system("pause");
+    return 0;
+}
